02e2a085d99c95478b12b42be1b2a5f30be2fd59
RannStudio <63898883+Rann-Studio@users.noreply.github.com>
2023-03-21 15:53:44 +0700

    Create retrieve_drop_magplant.lua

---
+++ script collection/retrieve_drop_magplant.lua
@@ -0,0 +1,61 @@
+-- Script by : Rann#6485
+-- Do not trade this script!
+
+local magPos = "top"
+local magOffset = 1
+local itemID = 1656
+local amountDrop = 250
+
+
+function retrieve_magplant(t)
+    local position = t[1] or t.position or exit_message{message = "position is not defined"}
+    local offset = t[2] or t.offset or nil
+
+    local magplant_x
+    local magplant_y
+
+    if (position == "top") then
+        magplant_x = (GetLocal().posX // 32)
+        magplant_y = ((GetLocal().posY // 32) - 1) - ((offset ~= nil) and offset or 0)
+        
+    elseif (position == "bottom") then
+        magplant_x = (GetLocal().posX // 32)
+        magplant_y = ((GetLocal().posY // 32) + 1) + ((offset ~= nil) and offset or 0)
+        
+    elseif (position == "left") then
+        magplant_x = ((GetLocal().posX // 32) - 1) - ((offset ~= nil) and offset or 0)
+        magplant_y = (GetLocal().posY // 32)
+        
+    elseif (position == "right") then
+        magplant_x = ((GetLocal().posX // 32) + 1) + ((offset ~= nil) and offset or 0)
+        magplant_y = (GetLocal().posY // 32)
+    
+    end
+
+    SendPacket(2, "action|dialog_return\ndialog_name|magplant_edit\nx|" .. magplant_x .. "|\ny|" .. magplant_y .."|\nbuttonClicked|withdraw\n")
+end
+
+
+function drop_item(t)
+    local item_id = t[1] or t.item_id or exit_message{message = "item id is not defined"}
+    local amount = t[2] or t.amount or exit_message{message = "amount is not defined"}
+
+    SendPacket(2, "action|dialog_return\ndialog_name|drop\nitem_drop|".. item_id .. "|\nitem_count|"..amount.."\n")
+end
+
+
+function exit_message(t)
+    local message = t[1] or t.message or "Thanks for using this library"
+    LogToConsole(message)
+    return false
+end
+
+
+
+while true do
+  retrieve_magplant{position = magPos, offset = magOffset}
+  Sleep(1000)
+
+  drop_item{item_id = itemID, amount = amountDrop }
+  Sleep(5000)
+end
